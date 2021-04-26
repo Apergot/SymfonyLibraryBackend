bc859699b0874693764216d13102512abc7a78c3
Alejandro Perdomo <apergot95@gmail.com>
2021-04-26 12:22:48 +0100

    Our form is working fine and just added validator for book

--- src/Controller/Api/BooksController.php
+++ src/Controller/Api/BooksController.php
@@ -3,10 +3,12 @@
 namespace App\Controller\Api;
 
 use App\Entity\Book;
+use App\Form\Type\BookFormType;
 use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Request;
 
 class BooksController extends AbstractFOSRestController {
 
@@ -23,12 +25,17 @@ class BooksController extends AbstractFOSRestController {
      * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks=true)
      */
     public function postAction(
-        EntityManagerInterface $em
+        EntityManagerInterface $em,
+        Request $request
     ){
         $book = new Book();
-        $book -> setTitle('Installing and using FOS Rest Bundle');
-        $em -> persist($book);
-        $em -> flush();
-        return $book;
+        $form = $this -> createForm(BookFormType::class, $book);
+        $form -> handleRequest($request);
+        if ($form -> isSubmitted() && $form -> isValid()) {
+            $em -> persist($book);
+            $em -> flush();
+            return $book;
+        }
+        return $form;
     }
 }
--- src/Form/Type/BookFormType.php
+++ src/Form/Type/BookFormType.php
@@ -2,7 +2,8 @@
 
 namespace App\Form\Type;
 
-use Doctrine\DBAL\Types\TextType;
+use App\Entity\Book;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -11,8 +12,7 @@ class BookFormType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            -> add('title', TextType::class);
+        $builder -> add('title', TextType::class);
     }
 
     // let us associate the class which references the form.
@@ -21,5 +21,16 @@ class BookFormType extends AbstractType {
         $resolver -> setDefaults([
             'data_class' => Book::class
         ]);
+    }
+
+    //In order to avoid request format using {"book_form: {...}"} next two methods
+    public function getBlockPrefix()
+    {
+        return '';
+    }
+
+    public function getName()
+    {
+        return '';
     }
 }
